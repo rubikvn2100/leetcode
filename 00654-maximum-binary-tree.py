32d3aecd98c9b37e1038652752b645fd661e80ca
Khoa Le <rubikvn2100@gmail.com>
2022-08-16 12:17:05 -0500

    Solved maximum-binary-tree using recursion method. Optimize by using Segment Tree to search for max value and index in O(log(n)). But, the list is to short for this one to work fast.

--- 00654-maximum-binary-tree.py
+++ 00654-maximum-binary-tree.py
@@ -6,18 +6,60 @@
 #         self.right = right
 class Solution:
     def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
-        n = len(nums)
-        if n == 0:
-            return None
-        
-        max_val = max(nums)
-        max_idx = nums.index(max_val)
+        def constructSegmentTree(start: int, end: int) -> Optional[TreeNode]:
+            # Each node val will contain:
+            # 1/ Start index of the segment
+            # 2/ Ending index of the segment
+            # 3/ Max value in the sement
+            if start > end:
+                return None 
+            
+            if start == end:
+                return TreeNode([start, start, nums[start], start])
 
-                
-        print(nums, max_val, max_idx)
-                
-        lst = None if max_idx == 0     else self.constructMaximumBinaryTree(nums[0:max_idx]    )
-        rst = None if max_idx == n - 1 else self.constructMaximumBinaryTree(nums[max_idx + 1:n])
+            mid = (start + end) >> 1 
+            lst = constructSegmentTree(start  , mid)
+            rst = constructSegmentTree(mid + 1, end)            
+            
+            _, _, max_val, max_idx = lst.val
+            if max_val < rst.val[2]:
+                _, _, max_val, max_idx = rst.val
+            
+            return TreeNode([start, end, max_val, max_idx], lst, rst)
         
-        return TreeNode(max_val, lst, rst)
+        def findMax(root: Optional[TreeNode], start: int, end: int) -> List[int]:
+            if not root:
+                return [-10**9, None]
+            
+            segment_start, segment_end, segment_max_val, segment_max_idx = root.val
+            if segment_end < start or end < segment_start:
+                return [-10**9, None]
+            
+            if start <= segment_start and segment_end <= end:
+                return [segment_max_val, segment_max_idx]
+            
+            lst_max_val, lst_max_idx = findMax(root.left , start, end)
+            rst_max_val, rst_max_idx = findMax(root.right, start, end)
+            if lst_max_val > rst_max_val:
+                return [lst_max_val, lst_max_idx]
+            else:
+                return [rst_max_val, rst_max_idx]
+        
+        maxSegmentTree = constructSegmentTree(0, len(nums) - 1)
+        def helper(start: int, end: int) -> Optional[TreeNode]:
+            if start > end:
+                return None
+            
+            if start == end:
+                return TreeNode(nums[start], None, None)
+                
+            max_val, max_idx = findMax(maxSegmentTree, start, end)
+            
+            lst = None if max_idx == start else helper(start      , max_idx - 1)
+            rst = None if max_idx == end   else helper(max_idx + 1, end        )
+            
+            return TreeNode(max_val, lst, rst)
+        
+        return helper(0, len(nums) - 1)
+            
         
