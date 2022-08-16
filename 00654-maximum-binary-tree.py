8efd257e67eedbf02f842fc6a1c0eb823dffe5b0
Khoa Le <rubikvn2100@gmail.com>
2022-08-16 11:33:27 -0500

    Solved maximum-binary-tree using recursion method. Run time is O(n^2) and can be further optimize to O(n*log(n)) if we use segment tree to help finding the max value in a range

---
+++ 00654-maximum-binary-tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
+        n = len(nums)
+        if n == 0:
+            return None
+        
+        max_val = nums[0]
+        max_idx =      0
+        for i, num in enumerate(nums):
+            if num > max_val: 
+                max_val = num
+                max_idx = i
+                
+        print(nums, max_val, max_idx)
+                
+        lst = None if max_idx == 0     else self.constructMaximumBinaryTree(nums[0:max_idx]    )
+        rst = None if max_idx == n - 1 else self.constructMaximumBinaryTree(nums[max_idx + 1:n])
+        
+        return TreeNode(max_val, lst, rst)
+        
