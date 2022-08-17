fd40f6f90df4f4be1425e9dff1c527073dae4c5c
Khoa Le <rubikvn2100@gmail.com>
2022-08-17 11:53:15 -0500

    Solve permutation-in-string by using hashmap, sliding window, and using a diff counter to keep track of how many freq diff between two hashmap to prevent the checking the whole hashmap for freq diff

--- 00567-permutation-in-string.py
+++ 00567-permutation-in-string.py
@@ -1,31 +1,44 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
-            return []
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        n = len(s1)
+        m = len(s2)
+        if n > m:
+            return False
         
-        current_level = [root]
-        result_lists  = [[root.val]]
-        while True:
-            next_level = []
-            for p in current_level:
-                if p.left:
-                    next_level.append(p.left)
-                if p.right:
-                    next_level.append(p.right)
-                    
-            if len(next_level) == 0:
-                break
-            else:
-                result_lists.append([p.val for p in next_level])
-                current_level = next_level
-            
-        return result_lists
-            
-                    
+        freq_dict_s1     = {char: 0 for char in s1 + s2}
+        freq_dict_sub_s2 = {char: 0 for char in s1 + s2}
+        for i in range(n):
+            freq_dict_s1[s1[i]]     += 1
+            freq_dict_sub_s2[s2[i]] += 1
+                
+        freq_diff_counter = 0
+        for char in freq_dict_s1.keys():
+            if freq_dict_s1[char] != freq_dict_sub_s2[char]:
+                freq_diff_counter += 1
         
+        if freq_diff_counter == 0:
+            return True
+                
+        k = n
+        while k < m:
+            remove_char = s2[k - n]
+            if freq_dict_s1[remove_char] == freq_dict_sub_s2[remove_char]:
+                freq_diff_counter += 1
+                
+            freq_dict_sub_s2[remove_char] -= 1
+            if freq_dict_s1[remove_char] == freq_dict_sub_s2[remove_char]:
+                freq_diff_counter -= 1
+                
+            add_char = s2[k]
+            if freq_dict_s1[add_char] == freq_dict_sub_s2[add_char]:
+                freq_diff_counter += 1
+                
+            freq_dict_sub_s2[add_char] += 1
+            if freq_dict_s1[add_char] == freq_dict_sub_s2[add_char]:
+                freq_diff_counter -= 1
+                             
+            if freq_diff_counter == 0:
+                return True 
+            k += 1
+                             
+        return False
