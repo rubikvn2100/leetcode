66131d7912d5dd488ffa2e1ae44511f7900f904a
Khoa Le <rubikvn2100@gmail.com>
2022-08-17 18:25:18 -0500

    Solved longest-zigzag-path-in-a-binary-tree with pre-processing that help prevent matching if the current binary tree is not deep enough

--- 01367-linked-list-in-binary-tree.py
+++ 01367-linked-list-in-binary-tree.py
@@ -11,54 +11,45 @@
 #         self.right = right
 class Solution:
     def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
-        def hasPathFromRoot(head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
-            if not head:
-                return True
+        def longestPath(head: Optional[ListNode], root: Optional[TreeNode]) -> int:
+            if not head and not root:
+                return 0
             
+            if head:
+                head.val = [head.val, longestPath(head.next, None) + 1]
+                return head.val[1]
+                            
+            if root:
+                lst_result = longestPath(None, root.left )
+                rst_result = longestPath(None, root.right)
+                root.val = [root.val, max(lst_result, rst_result) + 1]
+                return root.val[1]
+                       
+        def helper(head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
+            def hasPathFromRoot(head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
+                if not head:
+                    return True
+
+                if not root:
+                    return False
+
+                if head.val[0] != root.val[0]:
+                    return False
+
+                if head.val[1] > root.val[1]:
+                    print("stop")
+                    return False
+
+                return hasPathFromRoot(head.next, root.left) or hasPathFromRoot(head.next, root.right)
+
             if not root:
                 return False
-            
-            if head.val != root.val:
-                return False
-            
-            return hasPathFromRoot(head.next, root.left) or hasPathFromRoot(head.next, root.right)
-        
-        if not root:
-            return False
-        
-        if hasPathFromRoot(head, root):
-            return True
-        
-        return self.isSubPath(head, root.left) or self.isSubPath(head, root.right)
-        # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
-        def hasPathFromRoot(head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
-            if not head:
+
+            if hasPathFromRoot(head, root):
                 return True
             
-            if not root:
-                return False
+            return helper(head, root.left) or helper(head, root.right)
             
-            if head.val != root.val:
-                return False
-            
-            return hasPathFromRoot(head.next, root.left) or hasPathFromRoot(head.next, root.right)
-        
-        if not root:
-            return False
-        
-        if hasPathFromRoot(head, root):
-            return True
-        
-        return self.isSubPath(head, root.left) or self.isSubPath(head, root.right)
+        longestPath(head, None)
+        longestPath(None, root)
+        return helper(head, root)
