a1872fe58a8f64f9731f7dd9757dcd7b0365a402
Khoa Le <rubikvn2100@gmail.com>
2022-08-17 18:08:21 -0500

    Solved linked-list-in-binary-tree by recurviely match the linked list with the current subtree

---
+++ 01367-linked-list-in-binary-tree.py
@@ -0,0 +1,64 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
+        def hasPathFromRoot(head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
+            if not head:
+                return True
+            
+            if not root:
+                return False
+            
+            if head.val != root.val:
+                return False
+            
+            return hasPathFromRoot(head.next, root.left) or hasPathFromRoot(head.next, root.right)
+        
+        if not root:
+            return False
+        
+        if hasPathFromRoot(head, root):
+            return True
+        
+        return self.isSubPath(head, root.left) or self.isSubPath(head, root.right)
+        # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
+        def hasPathFromRoot(head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
+            if not head:
+                return True
+            
+            if not root:
+                return False
+            
+            if head.val != root.val:
+                return False
+            
+            return hasPathFromRoot(head.next, root.left) or hasPathFromRoot(head.next, root.right)
+        
+        if not root:
+            return False
+        
+        if hasPathFromRoot(head, root):
+            return True
+        
+        return self.isSubPath(head, root.left) or self.isSubPath(head, root.right)
